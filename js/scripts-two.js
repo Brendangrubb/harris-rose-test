b1d62c1c75b5e68702a459a3f87f5e97bcbf6108
Brendan Grubb <brendangrubb@gmail.com>
2017-06-21 16:12:06 -0700

    add js to flip panels on wine bottle click

---
+++ js/scripts-two.js
@@ -0,0 +1,28 @@
+( function($) {
+
+  $(document).ready(function() {
+
+    $("#panel-two-wine").click(function() {
+      $(".panel-two").toggleClass("panel-flip");
+    });
+
+    $("#panel-three-wine").click(function() {
+      $(".panel-three").toggleClass("panel-flip");
+    });
+
+    $("#panel-four-wine").click(function() {
+      $(".panel-four").toggleClass("panel-flip");
+    });
+
+    $("#panel-five-wine").click(function() {
+      $(".panel-five").toggleClass("panel-flip");
+    });
+
+
+
+
+
+  });
+  // END (D)R(F)
+})(jQuery);
+// END JQUERY BODYGUARD
