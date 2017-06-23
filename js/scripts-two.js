5926c8d28ae0a11028792ee215ec19515958607c
Brendan Grubb <brendangrubb@gmail.com>
2017-06-22 19:01:04 -0700

    comment out all but panel one for detail work and cleaning up redundant styles

--- js/scripts-two.js
+++ js/scripts-two.js
@@ -1,26 +1,26 @@
 ( function($) {
 
   $(document).ready(function() {
-
-    $("#panel-two-wine").click(function() {
-      $(".panel-two-front").toggleClass("panel-flip");
-      $(".panel-two-front").toggleClass("panel-front-remove-y");
-      $(".panel-two-back").toggleClass("panel-back-remove-y");
-    });
-
-    $("#panel-three-wine").click(function() {
-      $(".panel-three").toggleClass("panel-flip");
-    });
-
-    $("#panel-four-wine").click(function() {
-      $(".panel-four").toggleClass("panel-flip");
-    });
-
-    $("#panel-five-wine").click(function() {
-      $(".panel-five").toggleClass("panel-flip");
-    });
-
-
+    //
+    // $("#panel-two-wine").click(function() {
+    //   $(".panel-two-front").toggleClass("panel-flip");
+    //   $(".panel-two-front").toggleClass("panel-front-remove-y");
+    //   $(".panel-two-back").toggleClass("panel-back-remove-y");
+    // });
+    //
+    // $("#panel-three-wine").click(function() {
+    //   $(".panel-three").toggleClass("panel-flip");
+    // });
+    //
+    // $("#panel-four-wine").click(function() {
+    //   $(".panel-four").toggleClass("panel-flip");
+    // });
+    //
+    // $("#panel-five-wine").click(function() {
+    //   $(".panel-five").toggleClass("panel-flip");
+    // });
+    //
+    //
 
 
 
