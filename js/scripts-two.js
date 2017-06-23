95d7db0b87b8b43ec342540203d3209ff00bf641
Brendan Grubb <brendangrubb@gmail.com>
2017-06-22 21:54:33 -0700

    add flip effect to panel 2

--- js/scripts-two.js
+++ js/scripts-two.js
@@ -1,24 +1,12 @@
 ( function($) {
 
   $(document).ready(function() {
-    
+
     $("#panel-two-wine").click(function() {
-      $(".panel-two-front").toggleClass("panel-flip");
-      $(".panel-two-front").toggleClass("panel-front-remove-y");
-      $(".panel-two-back").toggleClass("panel-back-remove-y");
+      $("#panel-two-front").toggleClass("panel-flip");
+      $("#panel-two-back").toggleClass("panel-flip-back");
     });
 
-    $("#panel-three-wine").click(function() {
-      $(".panel-three").toggleClass("panel-flip");
-    });
-
-    $("#panel-four-wine").click(function() {
-      $(".panel-four").toggleClass("panel-flip");
-    });
-
-    $("#panel-five-wine").click(function() {
-      $(".panel-five").toggleClass("panel-flip");
-    });
 
 
 
