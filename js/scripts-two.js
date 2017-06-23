0abea14a1fc4cbfbb95e361d3af751102072657b
Brendan Grubb <brendangrubb@gmail.com>
2017-06-23 10:17:03 -0700

    add script to pop up full text

--- js/scripts-two.js
+++ js/scripts-two.js
@@ -20,16 +20,36 @@
     });
 // Text Toggles
     $("#panel-text-one").click(function() {
-      alert("yoyoyo");
+      $("#panel-text-one").toggleClass("panel-text-visibility");
+      $("#panel-full-text-one").toggleClass("panel-text-visibility");
+    });
+    $("#panel-full-text-one").click(function() {
+      $("#panel-text-one").toggleClass("panel-text-visibility");
+      $("#panel-full-text-one").toggleClass("panel-text-visibility");
     });
     $("#panel-text-two").click(function() {
-      alert("yoyoyo");
+      $("#panel-text-two").toggleClass("panel-text-visibility");
+      $("#panel-full-text-two").toggleClass("panel-text-visibility");
+    });
+    $("#panel-full-text-two").click(function() {
+      $("#panel-text-two").toggleClass("panel-text-visibility");
+      $("#panel-full-text-two").toggleClass("panel-text-visibility");
     });
     $("#panel-text-three").click(function() {
-      alert("yoyoyo");
+      $("#panel-text-three").toggleClass("panel-text-visibility");
+      $("#panel-full-text-three").toggleClass("panel-text-visibility");
+    });
+    $("#panel-full-text-three").click(function() {
+      $("#panel-text-three").toggleClass("panel-text-visibility");
+      $("#panel-full-text-three").toggleClass("panel-text-visibility");
     });
     $("#panel-text-four").click(function() {
-      alert("yoyoyo");
+      $("#panel-text-four").toggleClass("panel-text-visibility");
+      $("#panel-full-text-four").toggleClass("panel-text-visibility");
+    });
+    $("#panel-full-text-four").click(function() {
+      $("#panel-text-four").toggleClass("panel-text-visibility");
+      $("#panel-full-text-four").toggleClass("panel-text-visibility");
     });
 
 
