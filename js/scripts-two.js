c9b8e12302416e614003f4931b087d5075aa4b44
Brendan Grubb <brendangrubb@gmail.com>
2017-06-22 23:17:46 -0700

    add content for flipped panel two

--- js/scripts-two.js
+++ js/scripts-two.js
@@ -1,10 +1,14 @@
 ( function($) {
 
   $(document).ready(function() {
-
+// Wine Toggles
     $("#panel-two-wine").click(function() {
       $("#panel-two-front").toggleClass("panel-flip");
       $("#panel-two-back").toggleClass("panel-flip-back");
+    });
+// Text Toggles
+    $("#panel-text-one").click(function() {
+      alert("yoyoyo");
     });
 
 
